53e80c5729382d8d399898ab32fffe6b7e363e2a
kushalvaghani <A00247163@mycambrian.ca>
2022-08-04 21:09:46 -0400

    Initial Commit

---
+++ MovieTime/MovieViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MovieViewController.swift
+//  MovieTime
+//
+//  Created by Cambrian on 2022-07-27.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    @IBOutlet weak var movieNameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ MovieTime/UserViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserViewController.swift
+//  MovieTime
+//
+//  Created by Cambrian on 2022-07-27.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
