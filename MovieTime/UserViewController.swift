4a06fa95c84b82d0dea3a130bd1cdb3621509650
kushalvaghani <A00247163@mycambrian.ca>
2022-08-04 22:11:53 -0400

    User VC that adds users
    updated UserVC that allows to add user to database (core data)

--- MovieTime/UserViewController.swift
+++ MovieTime/UserViewController.swift
@@ -6,15 +6,51 @@
 //
 
 import UIKit
+import CoreData
 
 class UserViewController: UIViewController {
 
+    var users: [User] = []
     @IBOutlet weak var userNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if let name = userNameTextField.text //if text field is not null
+        {
+            save(name: name) //name will be set
+        }
+    }
+    
+    //method parameter: name (String)
+    //Description: Save new user in core data
+    func save(name: String) {
+            
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            
+                return
+            }
+            
+            let managedContext = appDelegate.persistentContainer.viewContext
+            let entity = NSEntityDescription.entity(forEntityName: "User", in: managedContext)!
+            
+            let user = User(entity: entity, insertInto: managedContext)
+            
+            user.name = name
+            
+            do {
+                try managedContext.save()//saving text field's data to core data
+                users.append(user)
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+        }
+    
     
 
 }
