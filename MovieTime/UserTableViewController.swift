0a6faf0f839b4f07049b0f007052c3e79823cb28
kushalvaghani <A00247163@mycambrian.ca>
2022-08-05 02:00:18 -0400

    UserTableViewController
    user is able to check the added user to the table view, delete and added segue to the movies table view

--- MovieTime/UserTableViewController.swift
+++ MovieTime/UserTableViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import CoreData
 
 class UserTableViewController: UITableViewController {
 
+    var users: [User] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +21,26 @@ class UserTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+            guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+              
+            let managedContext = appDelegate.persistentContainer.viewContext
+              
+            let fetchRequest = NSFetchRequest<User>(entityName: "User")
+              
+            do {
+                users = try managedContext.fetch(fetchRequest)
+                self.tableView.reloadData() //reloading table after fetching user from core data
+            } catch let error as NSError {
+                print("was not able to get any data" + error.description)
+            }
+        }
 
     // MARK: - Table view data source
 
@@ -28,38 +51,54 @@ class UserTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return users.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UserCell
 
+        cell.nameLabel.text = users[indexPath.row].name
         // Configure the cell...
 
         return cell
     }
-    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let dst = segue.destination as? MovieTableViewController
+        {
+       
+        dst.user = users[tableView.indexPathForSelectedRow?.row ?? 0] //sending the selected user
+        }
+        
+    }
+    
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                // Delete the row from the data source
+                guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                    return
+                }
+                
+                let managedContext = appDelegate.persistentContainer.viewContext
+                managedContext.delete(users[indexPath.row])
+                appDelegate.saveContext()
+                
+                users.remove(at: indexPath.row)
+                self.tableView.reloadData()
+
+            } else if editingStyle == .insert {
+                // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+            }
+        }
 
     /*
     // Override to support rearranging the table view.
@@ -79,9 +118,11 @@ class UserTableViewController: UITableViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
 
+
+}
+
+class UserCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
 }
