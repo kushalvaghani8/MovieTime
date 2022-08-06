72aabdb265730dac33f365bb30dac6c4f230c982
kushalvaghani <A00247163@mycambrian.ca>
2022-08-05 21:41:00 -0400

    Movie Table VC
    Updated the movie table view controller so that it displays movies in a table view when the user is selected. Also, enabled the delete function that allows deleting movies for the selected user and added a class for the cell.

--- MovieTime/MovieTableViewController.swift
+++ MovieTime/MovieTableViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import CoreData
 
 class MovieTableViewController: UITableViewController {
 
+    var movies: [Movie] = []
+    var user: User!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +21,29 @@ class MovieTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+            guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+              
+            let managedContext = appDelegate.persistentContainer.viewContext
+              
+            let fetchRequest = NSFetchRequest<Movie>(entityName: "Movie")
+              
+            do {
+                movies = try managedContext.fetch(fetchRequest)
+                movies = movies.filter({ obj in
+                    obj.user == user //checking for user
+                })
+                self.tableView.reloadData() //reloading table view after receving movie data from  core data
+            } catch let error as NSError {
+                print("was not able to get any data" + error.description)
+            }
+        }
 
     // MARK: - Table view data source
 
@@ -28,38 +54,54 @@ class MovieTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return movies.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
 
         // Configure the cell...
+        cell.nameLabel.text = movies[indexPath.row].name
 
         return cell
     }
-    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dst = segue.destination as! MovieViewController
+        dst.user = user //sending user data to destination
+    }
+    
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                // Delete the row from the data source
+                guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                    return
+                }
+                
+                let managedContext = appDelegate.persistentContainer.viewContext
+                managedContext.delete(movies[indexPath.row])
+                appDelegate.saveContext()
+                
+                movies.remove(at: indexPath.row)
+                self.tableView.reloadData()
+
+            } else if editingStyle == .insert {
+                // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+            }
+        }
+    
 
     /*
     // Override to support rearranging the table view.
@@ -79,9 +121,10 @@ class MovieTableViewController: UITableViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
+}
 
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
 }
